48ad43cebb775ab93e6067e8d5b3c6961958dd95
Jessé Willian <jessewillian0@gmail.com>
2021-11-19 13:27:54 -0300

    Exerc[icio 2

---
+++ Lista 1/exe2.py
@@ -0,0 +1,15 @@
+#2. Faça um programa que:
+#-receba quatro números inteiros
+#-calcule e mostre a soma desses números.
+# LEIA n1, n2, n3, n4
+# soma = n1 + n2 + n3 + n4
+# ESCREVA soma
+#entrada
+n1 = int(input('Informe o primeiro númeroa: '))
+n2 = int(input('Informe o segundo número: '))
+n3 = int(input('Informe o terceiro número: '))
+n4 = int(input('Informe o quarto númeto: '))
+#processamento
+resultado = n1 + n2 + n3 + n4
+#saída
+print('Soma das médias: ',resultado)
