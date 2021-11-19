20bee6635d3c5c7f466ed3bf1b11413781f05e00
Jessé Willian <jessewillian0@gmail.com>
2021-11-19 13:22:18 -0300

    cod1.1

--- Lista 1/exe1.py
+++ Lista 1/exe1.py
@@ -1,3 +1,7 @@
+# 1. Faça um programa que leia um número, multiplique ele por 2 e apresente o resultado.
+  #LEIA numero
+  #resultado = numero * 2
+  #ESCREVA resultado
 #entrada
 numero = int(input('Informe um valor: '))
 #processamento
