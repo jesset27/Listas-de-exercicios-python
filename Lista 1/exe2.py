2aebeda5a1ba5c7c95aa8c48c0f0665233f2af1d
Jessé Willian <jessewillian0@gmail.com>
2021-11-19 13:30:47 -0300

    cod2.0

--- Lista 1/exe2.py
+++ Lista 1/exe2.py
@@ -5,7 +5,7 @@
 # soma = n1 + n2 + n3 + n4
 # ESCREVA soma
 #entrada
-n1 = int(input('Informe o primeiro númeroa: '))
+n1 = int(input('Informe o primeiro númeroa : '))
 n2 = int(input('Informe o segundo número: '))
 n3 = int(input('Informe o terceiro número: '))
 n4 = int(input('Informe o quarto númeto: '))
