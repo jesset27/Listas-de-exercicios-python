fe8b19f6dd882f404b6757662e5974c6bf9097ee
Jessé Willian <jessewillian0@gmail.com>
2021-11-23 22:41:53 -0300

    cod9.0

---
+++ Lista 1/exe9.py
@@ -0,0 +1,10 @@
+'''9. Faça um programa que calcule e mostre a área de um triângulo. 
+Sabe-se que: Área = (base * altura) / 2.'''
+
+#entrada
+base = float(input('Informe a base: '))
+altura = float(input('Informe a altura: '))
+#processamento
+area = (base * altura) / 2
+#saida
+print('Valor da area', area)
