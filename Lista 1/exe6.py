9b7570c117d3c40b0f955cc1536f396d6015af36
Jessé Willian <jessewillian0@gmail.com>
2021-11-23 22:32:48 -0300

    cod6.0

--- Lista 1/exe6.py
+++ Lista 1/exe6.py
@@ -1,7 +1,8 @@
 '''6. Faça um programa que receba o salário base de um funcionário, calcule e mostre o salário
  a receber, sabendo-se que o funcionário tem gratificação de 5% sobre o salário base e paga 
  imposto de 7% também sobre o salário base.'''
- #entrada
+ 
+#entrada
 salario = float(input('Informe o salario base: '))
 #processamento
 gratificacao = salario /100 * 5
