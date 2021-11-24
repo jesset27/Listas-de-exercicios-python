9a50fa4815410a8b635512b7347cf3daa8063c42
Jessé Willian <jessewillian0@gmail.com>
2021-11-23 22:43:19 -0300

    cod10.0

---
+++ Lista 1/exe10.py
@@ -0,0 +1,10 @@
+'''10. Faça um programa que calcule e mostre a área de um círculo. 
+Sabe-se que: Área = p * raio 2 (o número 2 significa potência, ou seja, raio é elevado ao
+ quadrado)'''
+
+#entrada
+raio = float(input('Indique o raio: '))
+#processamento
+area = 3.14 * raio ** 2
+#saida
+print('Área do circulo: ',area)
