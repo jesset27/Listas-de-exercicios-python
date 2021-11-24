326e583c451acab7b59230d132ffce89321bd963
Jessé Willian <jessewillian0@gmail.com>
2021-11-23 22:47:08 -0300

    cod12.0

---
+++ Lista 1/exe12.py
@@ -0,0 +1,10 @@
+'''12. Faça um programa que receba dois números maiores que zero, calcule e mostre um elevado ao
+ outro.'''
+
+#entrada
+n1 = int(input('Número: '))
+n2 = int(input('Elevado a: '))
+#processamento
+resultado = n1 ** n2
+#saida
+print( 'Número elevado: ',resultado)
